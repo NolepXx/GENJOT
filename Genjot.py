529d6aa216fe21c93eb93b8bd0c5d86ab06197b8
NOLEPXXX <131864137+NolepXx@users.noreply.github.com>
2023-06-22 14:35:20 +0700

    Update Genjot.py

--- Genjot.py
+++ Genjot.py
@@ -297,7 +297,7 @@ class Menu:
 		self.tol = Console()
 		prints(Panel(f"{K2}        {self.negara}",width=87,padding=(0,30),title=f"{M2}• {H2}• {K2}• {H2}Negara {M2}• {H2}• {K2}•",subtitle=f"{M2}• {H2}• {K2}• {H2}Version : VERSI 2.2{M2} • {H2}• {K2}•",style=f"{color_panel}"))
 		yonkou.append(Panel(f" {K2}Nama Akun       {P2}: {H2}{nama}\n {K2}Status Pengguna {P2}: {H2}PRIBADI\n {K2}Ip Address      {P2}: {H2}{self.ip}\n {K2}Tanggal         {P2}: {H2}{tgl}",width=43,padding=(0,2),title=f"{M2}• {H2}• {K2}• {K2}Info-Akun {M2}• {H2}• {K2}•",style=f"{color_panel}"))
-		yonkou.append(Panel(f" {K2}Recode By {P2}: {H2}DensXyz\n {K2}Github  {P2} : {H2}github.com\n{K2} Facebook {P2}: {H2}Dens\n{K2} Whatsapp {P2}: {H2}+62*************",width=43,padding=(0,2),title=f"{M2}• {H2}• {K2}• {K2}Info-Source-code {M2}• {H2}• {K2}•",style=f"{color_panel}"))
+		yonkou.append(Panel(f" {K2}Recode By {P2}: {H2}NolepXx\n {K2}Github  {P2} : {H2}github.com\NolepXx\n{K2} Facebook {P2}: {H2}NolepXxxx\n{K2} Whatsapp {P2}: {H2}+62*************",width=43,padding=(0,2),title=f"{M2}• {H2}• {K2}• {K2}Info-Source-code {M2}• {H2}• {K2}•",style=f"{color_panel}"))
 		self.jol.print(Columns(yonkou))
 		cetak(Panel(f"{H2}Sunshine ID, Rachel ID, Fall Xavier, Suwan Dundo",width=87,title=f"{H2}Thanks To",style=f"{color_panel}"))
 		prints(Panel(f"{H2}\t                           Daftar Menu",width=87,style=f"{color_panel}"))
@@ -785,7 +785,7 @@ class Lain:
 			self.tampil_cookie()
 		elif menu in["06","6"]:
 			Menu().menu()
-			exit(prints(Panel(f"""{H2}berhasil menghapus cookie, silahkan ketik ulang python zmbf.py""",width=87,style=f"{color_panel}")))
+			exit(prints(Panel(f"""{H2}berhasil menghapus cookie, silahkan ketik ulang python Genjot.py""",width=87,style=f"{color_panel}")))
 		else:
 			exit(prints(Panel(f"""{M2}🙏 mohon maaf fitur ini sedang dalam tahap perbaikan""",width=87,style=f"{color_panel}")))
 
